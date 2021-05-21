1a1c609f9e50c10735a845136d4ab22ac5b74142
halowe <haleylowe0@gmail.com>
2021-05-20 23:50:17 -0700

    completed server with for middle ware, static assets, routes, Mongo, and API server

--- server.js
+++ server.js
@@ -1,6 +1,28 @@
+//using react-router
+
 const express = require("express");
 
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//static assets
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+//add routes for API and view
+app.use(routes);
+
+//Connect to Mongo DB
+mongoose.connect(process.env.MONGO_URI || "mongodb//localhost");
+
+//Start the API server
+app.listen(PORT, function () {
+  console.log(`API Server listening on PORT ${PORT}`);
+});
